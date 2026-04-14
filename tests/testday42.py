08f8787fa6c9151fbee8557420d3ff85a99eef88
hyunwoo0122 <132898621+hyunwoo0122@users.noreply.github.com>
2026-04-14 09:49:32 +0900

    Delete tests directory

--- tests/testday42.py
+++
@@ -1,5 +0,0 @@
-def sorted_log(logs: list[str])->list[str]:
-    return sorted(set(logs))
-
-logs = ["ERROR", "INFO", "WARNING", "ERROR", "INFO", "CRITICAL"]
-print(sorted_log(logs))
--- tests/testday44.py
+++
@@ -1,9 +0,0 @@
-def delete_space(lines: list[str])-> list[str]:
-    result=[]
-    for line in lines:
-        if line != "":
-            result.append(line)
-    return result
-
-lines = ["hello", "", "world", "", "", "python"]
-print(delete_space(lines))
